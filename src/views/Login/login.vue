8850ed2a5e5c769234d9bf07c23c542fb060558a
WEBwyj <1124173295@qq.com>
2020-01-18 22:01:37 +0800

    login

---
+++ src/views/Login/login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="login">
+        <el-switch
+  v-model="value2"
+  active-color="#13ce66"
+  inactive-color="#ff4949">
+</el-switch>
+<el-row>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+</el-row>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'login',
+    data() {
+      return {
+        value1: true,
+        value2: true
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
