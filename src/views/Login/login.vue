c705eba337dbc804ddc4fcc91943cf138ee3aaaf
WEBwyj <1124173295@qq.com>
2020-01-18 23:13:26 +0800

    login2

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -1,35 +1,150 @@
 <template>
-    <div id="login">
-        <el-switch
-  v-model="value2"
-  active-color="#13ce66"
-  inactive-color="#ff4949">
-</el-switch>
-<el-row>
-  <el-button>默认按钮</el-button>
-  <el-button type="primary">主要按钮</el-button>
-  <el-button type="success">成功按钮</el-button>
-  <el-button type="info">信息按钮</el-button>
-  <el-button type="warning">警告按钮</el-button>
-  <el-button type="danger">危险按钮</el-button>
-</el-row>
+  <div id="login">
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          :class="{'current':item.current}"
+          v-for="item in menuTab"
+          :key="item.id"
+          @click="toggleMenu(item)"
+        >{{item.txt}}</li>
+      </ul>
 
+      <el-form
+        :model="ruleForm2"
+        status-icon
+        :rules="rules2"
+        ref="ruleForm2"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="邮箱" prop="username">
+          <el-input type="text" v-model="ruleForm2.username" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="ruleForm2.password" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="验证码" style="display:block;" prop="code">
+          <el-row :gutter="10">
+            <el-col :span="11">
+               <el-input v-model.number="ruleForm2.code"></el-input>
+            </el-col>
+            <el-col :span="12">
+              <el-button type="success" class="block">获取验证码</el-button>
+            </el-col>
+          </el-row>
+             </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'login',
-    data() {
-      return {
-        value1: true,
-        value2: true
+  name: "login",
+  data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("验证码不能为空"));
       }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (value < 18) {
+            callback(new Error("必须年满18岁"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else {
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm2.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      menuTab: [
+        { txt: "登录", current: true },
+        { txt: "注册", current: false }
+      ],
+      ruleForm2: {
+        username: "",
+        password: "",
+        code: ""
+      },
+      rules2: {
+        username: [{ validator: validatePass, trigger: "blur" }],
+        password: [{ validator: validatePass2, trigger: "blur" }],
+        code: [{ validator: checkAge, trigger: "blur" }]
+      }
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach((elem, index) => {
+        elem.current = false;
+      });
+      data.current = true;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+#login {
+  height: 100vh;
+  background-color: pink;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+}
+.block{
+    display: block;
+    width: 100%;
+}
 </style>
 
